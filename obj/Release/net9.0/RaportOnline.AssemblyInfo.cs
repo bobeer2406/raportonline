232552ab223a4402642d6c5a4a6eb32ad3719cdb
Piotr Komor (PKOM) <pkom@demant.com>
2025-05-20 14:42:45 +0200

    add wykres

--- obj/Release/net9.0/RaportOnline.AssemblyInfo.cs
+++ obj/Release/net9.0/RaportOnline.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RaportOnline")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7d11ba8f398797558170edf1c39ba77798070750")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a382566968f4776e1f99fc796b606f342386765")]
 [assembly: System.Reflection.AssemblyProductAttribute("RaportOnline")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RaportOnline")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
